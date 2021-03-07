1bc10ecace35a3d7e4521096ddad6b42d53ed8f7
vitorricoy <vitorricoy@gmail.com>
2021-03-06 21:26:18 -0300

    Comentários

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ int main() {
     vector<int> tempoViagem(n);
     // Declara o vetor que guarda o custo do bilhete de cada escala
     vector<double> custoBilhete(n);
-    // Soma tempo viagem
+    // Vetor de soma de prefixo para o cálculo do valor da soma de um intervalo em tempo constante
     vector<int> somaTempoViagem(n+1);
 
     // Lê os valores de desconto
@@ -42,26 +42,38 @@ int main() {
     for(int I=0; I<n; I++) {
         cin >> tempoViagem[I] >> custoBilhete[I];
     }
+
+    // Calcula o vetor de soma de prefixos do tempo das viagens
     somaTempoViagem[0] = 0;
     for(int I=1; I<=n; I++) {
         somaTempoViagem[I] = tempoViagem[I-1] + somaTempoViagem[I-1];
     }
 
+    // Declara a matriz usada para guardar os resultados da programação dinâmica
+    // Foi usada uma otimização de espaço em que apenas os dados da última escala é guardado
+    // Isso é possível pois é necessário apenas os dados da escala anterior para calcular os dados da seguinte
+    // Os casos base da equação de Bellman já estão cobertos pela inicialização com 0.0
     vector<vector<double> > dp(2, vector<double>(n+1, 0.0));
-
-    for(int ultPego = n-1; ultPego>=0; ultPego--) {
-        dp[n%2][ultPego] = 0;
-    }
 	
-    for(int at = n-1; at >= 0; at--) {
-        for(int ultPego = at; ultPego >= max(0, at-d+1); ultPego--) {
-            int numeroDescontosConsecutivos = at-ultPego;
-            int tempoUltimoDesconto = somaTempoViagem[at]-somaTempoViagem[ultPego];
-            dp[at%2][ultPego] = (1.0-descontoPercentual[numeroDescontosConsecutivos])*custoBilhete[at];
-            if(tempoUltimoDesconto+tempoViagem[at] >= t || numeroDescontosConsecutivos >= d-1) {
-                dp[at%2][ultPego] += dp[(at+1)%2][at+1];
+    // Implementa a equação de Bellman da solução do problema
+    for(int escalaAtual = n-1; escalaAtual >= 0; escalaAtual--) {
+        // Iteração acontece de escala atual até a primeira escala que pode ter um efeito na escala atual
+        int primeiraEscalaComEfeito = max(0, escalaAtual-d+1);
+        for(int primeiraEscalaDesconto = escalaAtual; primeiraEscalaDesconto >= primeiraEscalaComEfeito; primeiraEscalaDesconto--) {
+            // Número de escalas com desconto entre a escala atual e a escala em que o desconto se iniciou
+            int numeroDescontosConsecutivos = escalaAtual-primeiraEscalaDesconto;
+            // O tempo passado desde o embarque na primeira escala do desconto
+            int tempoUltimoDesconto = somaTempoViagem[escalaAtual]-somaTempoViagem[primeiraEscalaDesconto];
+            // Inicializa o custo da dp com o valor gasto na passagem da escala atual
+            dp[escalaAtual%2][primeiraEscalaDesconto] = (1.0-descontoPercentual[numeroDescontosConsecutivos])*custoBilhete[escalaAtual];
+            if(tempoUltimoDesconto+tempoViagem[escalaAtual] >= t || numeroDescontosConsecutivos >= d-1) {
+                // Caso não seja possível continuar o desconto acumulado para a próxima escala
+                // Adiciona o custo a partir da próxima escala iniciando um novo ciclo de descontos
+                dp[escalaAtual%2][primeiraEscalaDesconto] += dp[(escalaAtual+1)%2][escalaAtual+1];
             } else {
-                dp[at%2][ultPego] += min(dp[(at+1)%2][at+1], dp[(at+1)%2][ultPego]);
+                // Caso seja possível continuar o desconto acumulado para a próxima escala
+                // Escolhe entre iniciar um novo ciclo de descontos ou continuar com o desconto atual
+                dp[escalaAtual%2][primeiraEscalaDesconto] += min(dp[(escalaAtual+1)%2][escalaAtual+1], dp[(escalaAtual+1)%2][primeiraEscalaDesconto]);
             }
         }
     }
