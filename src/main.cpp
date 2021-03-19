5f7ab4d7db4ee2eb7a7da6d193bb8d77e9c665e9
vitorricoy <vitorricoy@gmail.com>
2021-03-19 19:45:56 -0300

    otimizada a dp

--- src/main.cpp
+++ src/main.cpp
@@ -63,30 +63,32 @@ int main() {
     // Foi usada uma otimização de espaço em que apenas os dados da última escala é guardado
     // Isso é possível pois é necessário apenas os dados da escala anterior para calcular os dados da seguinte
     // O caso base da equação de Bellman já está cobertos pela inicialização com 0.0
-    vector<vector<double> > pd(2, vector<double>(n+1, 0.0));
+    vector<vector<double> > pd(2, vector<double>(d, 0.0));
 	
     // Implementa a equação de Bellman da solução do problema
     for(int escalaAtual = n-1; escalaAtual >= 0; escalaAtual--) {
-        // Iteração acontece de escala atual até a primeira escala, que pode ser onde se iniciou o desconto
-        for(int escalaDesconto = escalaAtual; escalaDesconto >= 0; escalaDesconto--) {
-            // Número de escalas com desconto entre a escala atual e a escala em que o desconto se iniciou
-            int descontosJaUsados = escalaAtual-escalaDesconto;
+        // Itera pelos possíveis números de descontos já utilizados no ciclo de descontos atual
+        for(int descontosUsados = d-1; descontosUsados >= 0; descontosUsados--) {
+            // Calcula a escala em que o desconto se iniciou
+            int escalaDesconto = escalaAtual-descontosUsados;
+            if(escalaDesconto < 0) {
+                continue;
+            }
             // Calcula o tempo passado desde o embarque na primeira escala do desconto até o embarque na escala atual
             // Ou seja, a soma dos tempos de viagem do intervalo [escalaDesconto, escalaAtual)
             int tempoUltimoDesconto = calculaSomaIntervalo(escalaDesconto, escalaAtual-1, somaPrefixoTempo);
             
             // Verifica se o tempo gasto e o número de descontos entre escalaDesconto e escalaAtual
             // permite que o desconto seja usado
-            if(tempoUltimoDesconto < t && descontosJaUsados < d) {    
+            if(tempoUltimoDesconto < t) {    
                 // Inicializa o valor dessa instância com o valor gasto na passagem da escala atual
-                pd[escalaAtual%2][escalaDesconto] = (1.0-descontoPercentual[descontosJaUsados])*custoBilhete[escalaAtual];
+                pd[escalaAtual%2][descontosUsados] = (1.0-descontoPercentual[descontosUsados])*custoBilhete[escalaAtual];
                 // Escolhe entre iniciar um novo ciclo de descontos ou continuar com o desconto atual
-                pd[escalaAtual%2][escalaDesconto] += min(pd[(escalaAtual+1)%2][escalaAtual+1], pd[(escalaAtual+1)%2][escalaDesconto]);
+                pd[escalaAtual%2][descontosUsados] += min(pd[(escalaAtual+1)%2][0], pd[(escalaAtual+1)%2][descontosUsados+1]);
             } else {
-                // É impossível se ter um desconto para escalaAtual iniciado em escalaDesconto
+                // É impossível que a 'escalaAtual' tenha tido 'descontosUsados' descontos consecutivos
                 // Isso acontece pelo tempo de transporte entre essas escalas exceder o tempo máximo de um desconto
-                // ou por se ter atingido o número máximo de descontos consecutivos
-                pd[escalaAtual%2][escalaDesconto] = 1e20; // 1e20 usado como infinito
+                pd[escalaAtual%2][descontosUsados] = 1e20; // 1e20 usado como infinito
             }
         }
     }
